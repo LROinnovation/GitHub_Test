89513e3cbc273f1347df619691b8855d8c3fafaa
Guillaume Sheehy <guillaume.sheehy@polymtl.ca>
2017-04-24 15:24:12 -0400

    Update GitHub_test script
    Added secound red cloud of points

--- GitHub_test.m
+++ GitHub_test.m
@@ -7,12 +7,17 @@ clc
 %% Creating cloud of random points in 3d space
 
 Npoints = 100;
-Points_Pos = rand(100, 3);
+Cloud{1} = rand(100, 3);
+Cloud{2} = rand(100, 3);
 
 %% Plotting cloud
 
-plot3(Points_Pos(:, 1), Points_Pos(:, 2), Points_Pos(:, 3), '.');
+close all
+hold on
 
+for i=1:numel(Cloud)
+    plot3(Cloud{i}(:, 1), Cloud{i}(:, 2), Cloud{i}(:, 3), '.');
+end
 
 
 
